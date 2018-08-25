817807e0ac1cfaa51335ef92c36eb3aecb4a069e
Luke Policinski <luke@lukepolo.com>
2018-08-25 11:27:29 -0400

    removing remaining head code

--- views/layouts/Public.vue
+++ views/layouts/Public.vue
@@ -4,35 +4,3 @@
         <router-view></router-view>
     </div>
 </template>
-
-<script>
-export default {
-  head: {
-    meta: [
-      { name: "application-name", content: "Varie - A Vue.js Framework" },
-      {
-        name: "description",
-        content:
-          "A Vue.js Typescript Framework for building elegant Vue.js Applications",
-      },
-
-      // Twitter
-      { name: "twitter:title", content: "Varie - A Vue.js Framework" },
-      {
-        n: "twitter:description",
-        c:
-          "A Vue.js Typescript Framework for building elegant Vue.js Applications",
-      },
-
-      // Google+ / Schema.org
-      { itemprop: "name", content: "Varie - A Vue.js Framework" },
-      {
-        itemprop: "description",
-        content:
-          "A Vue.js Typescript Framework for building elegant Vue.js Applications",
-      },
-      { p: "og:image", c: "https://varie.io/img/varie-banner.png" },
-    ],
-  },
-};
-</script>
