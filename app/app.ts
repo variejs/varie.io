4b5b9d15d786242e003a8a51b2dc8e3e793a1961
Luke Policinski <luke.policinski@egov.com>
2018-09-18 17:20:54 -0400

    Fixing middleware with new varie update

--- app/app.ts
+++ app/app.ts
@@ -6,12 +6,22 @@
 |
 */
 import Vue from "vue";
+import * as Raven from 'raven-js';
 import { application } from "varie";
+import RavenVue from 'raven-js/plugins/vue';
 import BaseLayout from "varie/BaseLayout.vue";
 import RouterInterface from "varie/lib/routing/RouterInterface";
 import StateServiceInterface from "varie/lib/state/StateServiceInterface";
 
 application.boot().then((app) => {
+
+  if($config.get('env.ENV') === 'production') {
+    Raven
+      .config('https://e4a9bb84172c4b8bbed481291a6f3423@sentry.codepier.io/8')
+      .addPlugin(RavenVue)
+      .install();
+  }
+
   new Vue({
     render: (h) => h(BaseLayout),
     router: app.make<RouterInterface>("RouterService").getRouter(),
